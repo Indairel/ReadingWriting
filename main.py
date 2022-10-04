1ccdb82cbdf285a8c38cabf17ec14c288853466d
cyberindygos <cyberindygos@gmail.com>
2022-10-04 22:21:12 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+with open('test.txt', 'r') as rf:
+    with open('test_copy.txt', 'w') as wf:
+        for line in rf:
+            wf.write(line)
+
+with open('orion.jpg', 'rb') as rf:
+    with open('orion_copy.jpg', 'wb') as wf:
+        for line in rf:
+            wf.write(line)
+
+with open('orion.jpg', 'rb') as rf:
+    with open('orion_copy.jpg', 'wb') as wf:
+        chunk_size = 4096
+        rf_chunk = rf.read(chunk_size)
+        while len(rf_chunk) > 0:
+            wf.write(rf_chunk)
+            rf_chunk = rf.read(chunk_size)
